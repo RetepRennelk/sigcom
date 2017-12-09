0fea0887de7d11a092c1702ba239cc02d165b296
Peter Klenner <peterklenner@gmx.de>
2017-12-09 14:38:28 +0100

    Compute edge degree distribution of a parity check matrix

---
+++ sigcom/coding/degree_distribution.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+def degree_distribution(H):
+    vns = np.squeeze(np.array(H.sum(axis=0), np.int32))
+    vns_unique = np.unique(vns)
+    vn_stats = []
+    for vn in vns_unique:
+        vn_stats.append([vn, np.mean(vns == vn)])
+
+    cns = np.squeeze(np.array(H.sum(axis=1), np.int32))
+    cns_unique = np.unique(cns)
+    cn_stats = []
+    for cn in cns_unique:
+        cn_stats.append([cn, np.mean(cns == cn)])
+    return vn_stats, cn_stats
+
+
+if __name__ == '__main__':
+    from sigcom.coding.atsc.code_param_short import get
+    from sigcom.coding.PCM import PCM
+
+    cp = get([8, 15])
+    pcm = PCM(cp)
+
+    H = pcm.make_layered(True)
+    vn_stats, cn_stats = degree_distribution(H)
+
+    print(vn_stats)
+    print(' ')
+    print(cn_stats)
