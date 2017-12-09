6a7e27c153bba6d17933cfaab21b1bc1ac0df3e8
Peter Klenner <peterklenner@gmx.de>
2017-12-09 10:59:26 +0100

    Added class CodeParam as unified LDPC code interface

--- sigcom/coding/util.py
+++ sigcom/coding/util.py
@@ -1,79 +1,96 @@
 from scipy.sparse import csc_matrix, csr_matrix
 import numpy as np
+from collections import namedtuple
 
-def _pck_to_sparse_rows_and_cols(code):
+
+class CodeParam():
+    def __init__(self, pck, N, code_rate_id, nCyclicFactor):
+        self.N = N
+        self.pck = np.array([np.array(x, dtype=np.int32) for x in pck])
+        self.CR = code_rate_id[0] / code_rate_id[1]
+        self.K = int(N * self.CR)
+        self.code_rate_id = code_rate_id
+        self.nCyclicFactor = nCyclicFactor
+
+
+def _pck_to_sparse_rows_and_cols(cp):
     '''
-    Take the definition of the parity check matrices from DVB or ATSC 
+    cp is short for CodeParam
+
+    Take the definition of the parity check matrices from DVB or ATSC
     and translate them to a list representation of all edges.
     Call 'H = csc_matrix((np.ones(len(rows)),(rows,cols)),shape=(N-K,N))'
     afterwards to get a sparse matrix representation.
     '''
-    nLayers = int((code['N']-code['K'])/code['nCyclicFactor'])
+    nLayers = int((cp.N-cp.K)/cp.nCyclicFactor)
     rows = []
     cols = []
     col = 0
-    for row in code['pck']:
-        for m in range(code['nCyclicFactor']):
+    for row in cp.pck:
+        for m in range(cp.nCyclicFactor):
             for p in row:
-                idx = (p+m*nLayers) % (code['N']-code['K'])
+                idx = (p+m*nLayers) % (cp.N-cp.K)
                 rows.append(idx)
                 cols.append(col)
-            col+=1
-    for k in range(code['N']-code['K']-1):
+            col += 1
+    for k in range(cp.N-cp.K-1):
         rows.extend([k, k+1])
         cols.extend([col, col])
         col += 1
-    rows.append(code['N']-code['K']-1)
+    rows.append(cp.N-cp.K-1)
     cols.append(col)
     return rows, cols
 
-def make_pck(code):
-    rows, cols = _pck_to_sparse_rows_and_cols(code)
-    shape = (code['N']-code['K'], code['N'])
-    H = csc_matrix((np.ones(len(rows)), (rows,cols)), shape=shape)
+
+def make_pck(cp):
+    rows, cols = _pck_to_sparse_rows_and_cols(cp)
+    shape = (cp.N-cp.K, cp.N)
+    H = csc_matrix((np.ones(len(rows)), (rows, cols)), shape=shape)
     return H
-    
-def get_parity_interleaver(K, N = 64800, nCyclicFactor = 360):
-    parity = np.arange(N-K).reshape((nCyclicFactor,-1)).T.reshape(-1)
+
+
+def get_parity_interleaver(K, N=64800, nCyclicFactor=360):
+    parity = np.arange(N-K).reshape((nCyclicFactor, -1)).T.reshape(-1)
     return np.hstack((np.arange(K), parity+K))
-    
-def get_layerwise_pck(code, isParityPermuted):
+
+
+def get_layerwise_pck(cp, isParityPermuted):
     '''
     layerwise_pcks, diagOffsets = get_layerwise_pck(code, isParityPermuted)
     '''
     layerwise_pcks = []
-    N_layers = int((code['N']-code['K'])/code['nCyclicFactor'])
-    diagOffsets = np.zeros(N_layers)    
+    N_layers = int((cp.N-cp.K)/cp.nCyclicFactor)
+    diagOffsets = np.zeros(N_layers)
     for layer in range(N_layers):
         pck = []
-        for segment, ParityChecks in enumerate(code['pck']):
+        for segment, ParityChecks in enumerate(cp.pck):
             CurrentLayers = (ParityChecks - layer) % N_layers
             VerticalShifts = (ParityChecks[CurrentLayers == 0] - layer) / N_layers
             if len(VerticalShifts) > 0:
-                DiagonalShifts = (code['nCyclicFactor']-VerticalShifts) % code['nCyclicFactor']
-                ColumnOffset = segment*code['nCyclicFactor']
-                pck.extend([[ColumnOffset,int(ds)] for ds in DiagonalShifts])
+                DiagonalShifts = (cp.nCyclicFactor-VerticalShifts) % cp.nCyclicFactor
+                ColumnOffset = segment*cp.nCyclicFactor
+                pck.extend([[ColumnOffset, int(ds)] for ds in DiagonalShifts])
 
         if isParityPermuted:
-            pck.append([code['K']+code['nCyclicFactor']*layer, 0])
+            pck.append([cp.K+cp.nCyclicFactor*layer, 0])
             if layer == 0:
-                pck.append([code['N']-code['nCyclicFactor'], code['nCyclicFactor']-1])
+                pck.append([cp.N-cp.nCyclicFactor, cp.nCyclicFactor-1])
             else:
-                pck.append([code['K']+code['nCyclicFactor']*(layer-1), 0])
+                pck.append([cp.K+cp.nCyclicFactor*(layer-1), 0])
         else:
-            pck.append([code['K'], layer])
-            pck.append([code['K'], layer-1])
+            pck.append([cp.K, layer])
+            pck.append([cp.K, layer-1])
         layerwise_pcks.append(pck)
 
         if layer+1 < N_layers:
             diagOffsets[layer+1] = int(diagOffsets[layer] + len(pck))
-            
+
     layerwise_pcks = np.array([np.array(x, dtype=np.int32) for x in layerwise_pcks])
     diagOffsets = np.array(diagOffsets)
-    
     return layerwise_pcks, diagOffsets
-    
-def layerwise_pcks_to_PCM(layerwise_pcks, code):
+
+
+def layerwise_pcks_to_PCM(layerwise_pcks, cp):
     '''
     run 'layerwise_pcks, diagOffsets = get_layerwise_pck(code, isParityPermuted)'
     '''
@@ -81,25 +98,26 @@ def layerwise_pcks_to_PCM(layerwise_pcks, code):
     rows = []
     row = 0
     for i, pck in enumerate(layerwise_pcks):
-        for m in range(code['nCyclicFactor']):
-            x = pck[:,0] + (pck[:,1]+m) % code['nCyclicFactor']
-            if i==0 and m==0:
+        for m in range(cp.nCyclicFactor):
+            x = pck[:, 0] + (pck[:, 1] + m) % cp.nCyclicFactor
+            if (i == 0) and (m == 0):
                 x = x[:-1]
             cols.extend(x)
             rows.extend([row]*len(x))
             row += 1
-            
-    shape = (code['N']-code['K'], code['N'])
-    PCM = csr_matrix((np.ones(len(rows)), (rows,cols)), shape=shape)
+
+    shape = (cp.N-cp.K, cp.N)
+    PCM = csr_matrix((np.ones(len(rows)), (rows, cols)), shape=shape)
     return PCM
+
 
 if __name__ == '__main__':
     from sigcom.coding.atsc import pck_long
-    code = pck_long.get_pck([8,15])
-    H = make_pck(code)
+    codeParam = pck_long.get_pck([8, 15])
+    H = make_pck(codeParam)
     isParityPermuted = True
-    layerwise_pcks, diagOffsets = get_layerwise_pck(code, isParityPermuted)
-    PCM = layerwise_pcks_to_PCM(layerwise_pcks, code)
+    layerwise_pcks, diagOffsets = get_layerwise_pck(codeParam, isParityPermuted)
+    PCM = layerwise_pcks_to_PCM(layerwise_pcks, codeParam)
     import matplotlib.pyplot as plt
     plt.spy(PCM, markersize=.1)
     plt.show()
