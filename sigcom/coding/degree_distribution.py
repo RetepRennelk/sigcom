dbf3556fc393b073ec81c948797f27e88ad2a8cb
Peter Klenner <peterklenner@gmx.de>
2017-12-17 12:31:34 +0100

    Degree distribution uses list comprehension (as it should have)

--- sigcom/coding/degree_distribution.py
+++ sigcom/coding/degree_distribution.py
@@ -6,20 +6,19 @@ def degree_distribution(H):
 
     vns = np.squeeze(np.array(H.sum(axis=0), np.int32))
     vns_unique = np.unique(vns)
-    vn_stats = np.array([])
-    for vn in vns_unique:
-        np.append((vn_stats, np.array([vn, vn*np.sum(vns == vn)/N_edges])))
+    vn_stats = [np.array([vn, vn*np.sum(vns == vn)/N_edges])
+                for vn in vns_unique]
 
     cns = np.squeeze(np.array(H.sum(axis=1), np.int32))
     cns_unique = np.unique(cns)
-    cn_stats = np.array([])
-    for cn in cns_unique:
-        np.append((cn_stats, np.array([cn, cn*np.sum(cns == cn)/N_edges])))
-    return vn_stats, cn_stats
+    cn_stats = [np.array([cn, cn*np.sum(cns == cn)/N_edges])
+                for cn in cns_unique]
+
+    return np.array(vn_stats), np.array(cn_stats)
 
 
 if __name__ == '__main__':
-    from sigcom.coding.atsc.code_param_short import get
+    from sigcom.coding.atsc.code_param_long import get
     from sigcom.coding.PCM import PCM
 
     cp = get([8, 15])
