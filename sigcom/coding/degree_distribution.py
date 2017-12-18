7dad2c0c2892f5d9ea13eeb19fc037dc21089a07
Peter Klenner <peterklenner@gmx.de>
2017-12-18 07:52:39 +0100

    Added support for edge and node view conversion

--- sigcom/coding/degree_distribution.py
+++ sigcom/coding/degree_distribution.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def degree_distribution(H):
+def degree_distribution_edge_view(H):
     N_edges = H.sum().sum()
 
     vns = np.squeeze(np.array(H.sum(axis=0), np.int32))
@@ -17,6 +17,44 @@ def degree_distribution(H):
     return np.array(vn_stats), np.array(cn_stats)
 
 
+def degree_distribution_node_view(H):
+    vns = np.squeeze(np.array(H.sum(axis=0), np.int32))
+    vns_unique = np.unique(vns)
+    vn_stats = [np.array([vn, np.mean(vns == vn)])
+                for vn in vns_unique]
+
+    cns = np.squeeze(np.array(H.sum(axis=1), np.int32))
+    cns_unique = np.unique(cns)
+    cn_stats = [np.array([cn, np.mean(cns == cn)])
+                for cn in cns_unique]
+
+    return np.array(vn_stats), np.array(cn_stats)
+
+
+def edge_to_node_view(vn_edge_stats, cn_edge_stats):
+    vn_node_stats = vn_edge_stats[:, 1] / vn_edge_stats[:, 0]
+    vn_node_stats = vn_node_stats/vn_node_stats.sum()
+    vn_node_stats = np.vstack((vn_edge_stats[:, 0], vn_node_stats)).T
+
+    cn_node_stats = cn_edge_stats[:, 1] / cn_edge_stats[:, 0]
+    cn_node_stats = cn_node_stats/cn_node_stats.sum()
+    cn_node_stats = np.vstack((cn_edge_stats[:, 0], cn_node_stats)).T
+
+    return vn_node_stats, cn_node_stats
+
+
+def node_to_edge_view(vn_node_stats, cn_node_stats):
+    vn_edge_stats = vn_node_stats[:, 1] * vn_node_stats[:, 0]
+    vn_edge_stats = vn_edge_stats/vn_edge_stats.sum()
+    vn_edge_stats = np.vstack((vn_node_stats[:, 0], vn_edge_stats)).T
+
+    cn_edge_stats = cn_node_stats[:, 1] * cn_node_stats[:, 0]
+    cn_edge_stats = cn_edge_stats/cn_edge_stats.sum()
+    cn_edge_stats = np.vstack((cn_node_stats[:, 0], cn_edge_stats)).T
+
+    return vn_edge_stats, cn_edge_stats
+
+
 if __name__ == '__main__':
     from sigcom.coding.atsc.code_param_long import get
     from sigcom.coding.PCM import PCM
@@ -25,8 +63,24 @@ if __name__ == '__main__':
     pcm = PCM(cp)
 
     H = pcm.make_layered(True)
-    vn_stats, cn_stats = degree_distribution(H)
+    vn_edge_stats, cn_edge_stats = degree_distribution_edge_view(H)
+    vn_node_stats, cn_node_stats = degree_distribution_node_view(H)
+    
+    vn_node_stats2, cn_node_stats2 = edge_to_node_view(vn_edge_stats, cn_edge_stats)
+    vn_edge_stats2, cn_edge_stats2 = node_to_edge_view(vn_node_stats, cn_node_stats)
 
-    print(vn_stats)
+    print('vn_edge_stats\n', vn_edge_stats)
     print(' ')
-    print(cn_stats)
+    print('cn_edge_stats\n', cn_edge_stats)
+    print(' ')
+    print('vn_node_stats\n', vn_node_stats)
+    print(' ')
+    print('cn_node_stats\n', cn_node_stats)
+    print('----------------------------------------')
+    print('vn_edge_stats2\n', vn_edge_stats - vn_edge_stats2)
+    print(' ')
+    print('cn_edge_stats2\n', cn_edge_stats - cn_edge_stats2)
+    print(' ')
+    print('vn_node_stats2\n', vn_node_stats - vn_node_stats2)
+    print(' ')
+    print('cn_node_stats2\n', cn_node_stats - cn_node_stats2)
