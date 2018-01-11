4552a96c93e5e0f14ea74bd9bd63a2106e3ef031
Peter Klenner <peterklenner@gmx.de>
2018-01-11 08:35:06 +0100

    Converted peg functions to class.

--- sigcom/coding/peg/peg_x.py
+++ sigcom/coding/peg/peg_x.py
@@ -3,60 +3,72 @@ import numpy as np
 from networkx.algorithms.bipartite.matrix import biadjacency_matrix
 
 
-def find_cn(col, rows, sw_rand=1):
-    '''
-    Find check-node with smallest degree
-    '''
-    h_sum = np.zeros(M)
-    for i in range(col):
-        h_sum[np.array(g[-(i+1)])-1] += 1
-    # restrict candidates to rows
-    h_sum = h_sum[rows-1]
-    if sw_rand==0:
-        idx = np.argmin(h_sum)
-    else:
-        indices = np.where(h_sum==np.min(h_sum))[0]
-        idx = np.random.choice(indices)
-    return rows[idx]
+class Peg_X():
+    def __init__(self, M, N, dv, sw_rand):
+        self.M = M
+        self.N = N
+        self.dv = dv
+        self.sw_rand = sw_rand
+        self.g = nx.Graph()
+        self.init()
+
+    def init(self):
+        self.vn_nodes = -np.arange(1, self.N+1)
+        self.cn_nodes = +np.arange(1, self.M+1)
+        self.g.add_nodes_from(self.vn_nodes, bipartite=0)  # bit-nodes
+        self.g.add_nodes_from(self.cn_nodes, bipartite=1)  # check-nodes
+
+    def tosparse(self):
+        return biadjacency_matrix(self.g, self.cn_nodes, self.vn_nodes)
+
+    def run(self):
+        for col in range(self.N):
+            idx = self.find_cn(col, self.cn_nodes)
+            self.g.add_edge(idx, -(col+1))
+            for d in range(1, self.dv):
+                cn_cands = self.bfs(col)
+                idx = self.find_cn(col, cn_cands)
+                self.g.add_edge(idx, -(col+1))
+
+    def find_cn(self, col, rows):
+        '''
+        Find check-node with smallest degree
+        '''
+        h_sum = np.zeros(self.M)
+        for i in range(col):
+            h_sum[np.array(self.g[-(i+1)])-1] += 1
+            # restrict candidates to rows
+        h_sum = h_sum[rows-1]
+        if self.sw_rand == 0:
+            idx = np.argmin(h_sum)
+        else:
+            indices = np.where(h_sum == np.min(h_sum))[0]
+            idx = np.random.choice(indices)
+        return rows[idx]
+
+    def bfs(self, col):
+        l0 = np.array(list(self.g[-(col+1)]))
+        l2 = np.array([], dtype=np.int32)
+        while 1:
+            l1 = np.array([])
+            for x in l0:
+                l1 = np.append(l1, list(self.g[x]))
+            for x in np.unique(l1):
+                l2 = np.append(l2, list(self.g[x]))
+            l2 = np.unique(l2)
+            N0 = l0.size
+            N1 = l2.size
+            if (N0 < self.M and N1 == self.M) or (N0 >= N1 and N0 < self.M):
+                break
+            l0 = l2.copy()
+        cn_cands = np.setdiff1d(self.cn_nodes, l0)
+        return cn_cands
 
 
-def bfs(col, dbg=0):
-    l0 = np.array(list(g[-(col+1)]))
-    l2 = np.array([], dtype=np.int32)
-    while 1:
-        l1 = np.array([])
-        for x in l0:
-            l1 = np.append(l1, list(g[x]))
-        for x in np.unique(l1):
-            l2 = np.append(l2, list(g[x]))
-        l2 = np.unique(l2)
-        N0 = l0.size
-        N1 = l2.size
-        if (N0 < M and N1 == M) or (N0 >= N1 and N0 < M):
-            break
-        l0 = l2.copy()
-    cn_cands = np.setdiff1d(cn_nodes, l0)
-    return cn_cands
-
-
-M = 10
-N = 10
-dv = 3
-g = nx.Graph()
-
-vn_nodes = -np.arange(1, N+1)
-cn_nodes = +np.arange(1, M+1)
-
-g.add_nodes_from(vn_nodes, bipartite=0)  # bit-nodes
-g.add_nodes_from(cn_nodes, bipartite=1)  # check-nodes
-
-for col in range(N):
-    idx = find_cn(col, cn_nodes)
-    g.add_edge(idx, -(col+1))
-    for d in range(1,dv):
-        cn_cands = bfs(col)
-        idx = find_cn(col, cn_cands)
-        g.add_edge(idx, -(col+1))
-
-mtx = biadjacency_matrix(g, cn_nodes, vn_nodes)
-print(mtx.todense())
+if __name__ == '__main__':
+    M = 10
+    N = 10
+    dv = 3
+    peg_x = Peg_X(M, N, dv, sw_rand=1)
+    peg_x.run()
+    print(peg_x.tosparse().todense())
