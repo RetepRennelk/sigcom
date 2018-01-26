7cf6f4229f8adf701800d9967e1d8be1001871c7
Peter Klenner <peterklenner@gmx.de>
2018-01-26 17:03:31 +0100

    Added Hermite Polynomial.

--- sigcom/util/general.py
+++ sigcom/util/general.py
@@ -22,6 +22,22 @@ def bits_to_ints(bits, bw):
     return np.array(ints, np.int)
 
 
+def hermite_poly(n):
+    '''
+    H0(x) = 1
+    H1(x) = 2x
+    H_(n)(x) = 2*x*H_(n-1)(x) - 2*(n-1)*H_(n-2)(x)
+    '''
+    if n == 0:
+        return np.array([1])
+    elif n == 1:
+        return np.array([2, 0])
+    else:
+        rhs = np.append(hermite_poly(n-1), 0)
+        rhs[2:] -= (n-1)*hermite_poly(n-2)
+        return 2*rhs
+
+
 if __name__ == '__main__':
     ints = range(5)
     bits = ints_to_bits(ints, 3)
