997f7e35f26b5cd01a7a882f0ca68ba0adc857db
Peter Klenner <peterklenner@gmx.de>
2018-01-27 14:37:22 +0100

    From and to Mutual Information and Power.

--- sigcom/util/general.py
+++ sigcom/util/general.py
@@ -67,7 +67,47 @@ class MI_Gauss_Hermite():
         self.func = lambda P: 1-.5*(func_helper(xi, +1, P)+func_helper(xi, -1, P)).dot(wi)
 
     def get(self, P):
-        return np.array([self.func(p) for p in np.atleast_1d(P)])
+        return np.array([self.func(4/p) for p in np.atleast_1d(P)])
+
+    def getMutualInfo(self, P):
+        def helper(P):
+            nSigma = np.sqrt(P)
+            if nSigma >= 10:
+                Ia = 1.
+            elif nSigma <= 1.6363:
+                a = -.0421061
+                b = .209252
+                c = -.00640081
+                Ia = a*nSigma**3 + b*nSigma**2 + c*nSigma
+            else:
+                a = .00181491
+                b = -.142675
+                c = -.0822054
+                d = .0549608
+                Ia = 1-np.exp(a*nSigma**3 + b*nSigma**2 + c*nSigma + d)
+            return Ia
+        return np.array([helper(p) for p in np.atleast_1d(P)])
+
+    def getNoisePower(self, Ia):
+        def helper(Ia):
+            if Ia <= .3646:
+                a = 1.09542
+                b = .214217
+                c = 2.33727
+                Pa = (a*Ia**2 + b*Ia + c*np.sqrt(Ia))**2
+            else:
+                a = .706692
+                b = .386013
+                c = -1.75017
+                Pa = (-a*np.log(b*(1-Ia)) - c*Ia)**2
+            return Pa
+        return np.array([helper(ia) for ia in np.atleast_1d(Ia)])
+
+    def plot(self):
+        import matplotlib.pyplot as plt
+        P_dB = np.linspace(-40, 40, 100)
+        plt.plot(P_dB, self.get(10**(P_dB/10))-self.getMutualInfo(10**(P_dB/10)))
+        plt.show()
 
 
 if __name__ == '__main__':
