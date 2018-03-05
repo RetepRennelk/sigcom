73dce77daacba403083d64134acc16c9cc18595c
Peter Klenner <peterklenner@gmx.de>
2018-03-05 07:24:03 +0100

    Superposition modulation tx, added convenience.

--- sigcom/tx/spm.py
+++ sigcom/tx/spm.py
@@ -1,16 +1,20 @@
-from numpy import log2, sqrt, exp, pi
+from numpy import log2, sqrt, exp, pi, array
 from numpy.random import rand
 from sigcom.tx.util import generate_bits, qam_alphabet, \
     map_bits_to_symbol_alphabet
 
 
 class SP1p4():
+    '''
+    tx = sqrt(P0)*tx0 + sqrt(P1)*tx1*exp(j*phi)
+    '''
     def __init__(self, N_cells):
         self.M = 4
         self.ldM = log2(self.M)
         self.X0 = qam_alphabet(self.M)
         self.X1 = qam_alphabet(self.M)
         self.N_cells = N_cells
+        self.update()
 
     def update(self):
         N_bits = int(self.ldM*self.N_cells)
@@ -24,4 +28,5 @@ class SP1p4():
         '''
         Powers : Ps =  [P0, P1]
         '''
+        self.Ps = array(Ps)
         self.tx = sqrt(Ps[0])*self.tx0 + sqrt(Ps[1])*self.tx1*self.phase
