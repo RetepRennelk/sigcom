7027d4a9c830888441c762b56f47b0de0363b8c0
Peter Klenner <peterklenner@gmx.de>
2018-03-10 11:06:01 +0100

    Moved modcod to tx package and added convenience class.

---
+++ sigcom/tx/modcod.py
@@ -0,0 +1,81 @@
+import numpy as np
+from sigcom.tx.util import qam_alphabet, \
+    generate_bits, map_bits_to_symbol_alphabet
+from sigcom.coding.atsc import bititlv_short, bititlv_long
+from sigcom.coding.atsc import code_param_long, code_param_short
+from sigcom.coding.PCM import PCM
+from sigcom.coding.util import get_parity_interleaver
+
+
+class ModCodAtsc():
+    def __init__(self, M, CR, N_ldpc):
+        self.M = M
+        self.CR = CR
+        self.N_ldpc = N_ldpc
+        self.N_fec_cells = int(N_ldpc / np.log2(M))
+
+        if M == 4:
+            self.X = qam_alphabet(M)
+        else:
+            assert False, "Implement me!"
+
+        if N_ldpc == 16200:
+            self.bil = bititlv_short.bititlv_short(M, CR)
+            self.cp = code_param_short.get(CR)
+        else:
+            self.bil = bititlv_long.bititlv_long(M, CR)
+            self.cp = code_param_long.get(CR)
+
+        self.pcm = PCM(self.cp)
+        self.H_enc = self.pcm.make()
+        self.H_dec = self.pcm.make_layered(True)
+
+        self.parintl = get_parity_interleaver(self.cp.K)
+
+    def generate(self, N_codewords):
+        self.bits = generate_bits(N_codewords*self.cp.K)
+        self.m_bits = self.bits.reshape(-1, self.cp.K).T
+        parity = np.int64(self.H_enc[:, :self.cp.K].dot(self.m_bits))
+        parity = np.cumsum(parity, axis=0) % 2
+        self.m_codebits = np.vstack((self.m_bits, parity))[self.parintl, :]
+        self.m_codebits_biled = self.m_codebits[self.bil, :]
+        c = self.m_codebits_biled.T.flatten()
+        self.tx = map_bits_to_symbol_alphabet(c, self.X)
+
+
+class ModCodSP1p4():
+    def __init__(self, M, CR, N_ldpc):
+        self.tx0 = ModCodAtsc(M, CR, N_ldpc)
+        self.tx1 = ModCodAtsc(M, CR, N_ldpc)
+
+    def update(self, N_codewords):
+        self.tx0.generate(N_codewords)
+        self.tx1.generate(N_codewords)
+        N_cells = len(self.tx0.tx)
+        self.phase = np.exp(1j*2*np.pi*np.random.rand(N_cells))
+
+    def generate(self, Ps):
+        self.tx = np.sqrt(Ps[0]) * self.tx0.tx \
+                  + np.sqrt(Ps[1]) * self.tx1.tx * self.phase
+
+
+class ModCodSP1p4_rs():
+    def __init__(self, M, CR, N_ldpc):
+        self.tx0 = ModCodAtsc(M, CR, N_ldpc)
+        self.tx1 = ModCodAtsc(M, CR, N_ldpc)
+
+    def update(self, N_codewords):
+        self.tx0.generate(N_codewords)
+        self.tx1.generate(N_codewords)
+        N_cells = len(self.tx0.tx)
+        self.phase = np.exp(1j*2*np.pi*np.random.rand(N_cells))
+
+    def generate(self, Ps):
+        N_cells = self.tx0.N_fec_cells
+        m_tx0 = self.tx0.tx.reshape(-1, N_cells).copy()
+        m_tx1 = self.tx1.tx.reshape(-1, N_cells).copy()
+        m_tx0[:, :int(N_cells/2)] *= np.sqrt(Ps[0])
+        m_tx1[:, :int(N_cells/2)] *= np.sqrt(Ps[1])
+        m_tx0[:, int(N_cells/2):] *= np.sqrt(Ps[1])
+        m_tx1[:, int(N_cells/2):] *= np.sqrt(Ps[0])
+        self.tx = m_tx0.flatten() + m_tx1.flatten() * self.phase
