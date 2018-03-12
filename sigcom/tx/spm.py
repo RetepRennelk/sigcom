495cf6b642b33df10a6e364dd28f6a219ac59ae1
Peter Klenner <peter.klenner@eu.panasonic.com>
2018-03-12 09:33:08 +0100

    Added ModCods.

--- sigcom/tx/spm.py
+++ sigcom/tx/spm.py
@@ -2,6 +2,8 @@ from numpy import log2, sqrt, exp, pi, array
 from numpy.random import rand
 from sigcom.tx.util import generate_bits, qam_alphabet, \
     map_bits_to_symbol_alphabet
+from sigcom.tx.mod_cod_atsc import ModCodAtsc
+import numpy as np
 
 
 class SP1p4():
@@ -30,3 +32,42 @@ class SP1p4():
         '''
         self.Ps = array(Ps)
         self.tx = sqrt(Ps[0])*self.tx0 + sqrt(Ps[1])*self.tx1*self.phase
+
+
+class ModCodSP1p4():
+    def __init__(self, M, CR, N_ldpc):
+        self.tx0 = ModCodAtsc(M, CR, N_ldpc)
+        self.tx1 = ModCodAtsc(M, CR, N_ldpc)
+
+    def update(self, N_codewords):
+        self.tx0.generate(N_codewords)
+        self.tx1.generate(N_codewords)
+        N_cells = len(self.tx0.tx)
+        self.phase = exp(1j*2*pi*rand(N_cells))
+
+    def generate(self, Ps):
+        self.tx = np.sqrt(Ps[0])*self.tx0.tx + np.sqrt(Ps[1])*self.tx1.tx*self.phase
+
+
+class ModCodSP1p4_rs():
+    def __init__(self, M, CR, N_ldpc):
+        self.N_fec_cells = int(N_ldpc / np.log2(M))
+        self.tx0 = ModCodAtsc(M, CR, N_ldpc)
+        self.tx1 = ModCodAtsc(M, CR, N_ldpc)
+
+    def update(self, N_codewords):
+        self.tx0.generate(N_codewords)
+        self.tx1.generate(N_codewords)
+        N_cells = len(self.tx0.tx)
+        self.phase = exp(1j*2*pi*rand(N_cells))
+
+    def generate(self, Ps):
+        #import ipdb; ipdb.set_trace()
+        N_fec_cells = self.N_fec_cells
+        m_tx0 = self.tx0.tx.reshape(-1, N_fec_cells).copy()
+        m_tx1 = self.tx1.tx.reshape(-1, N_fec_cells).copy()
+        m_tx0[:,:int(N_fec_cells/2)] *= np.sqrt(Ps[0])
+        m_tx1[:,:int(N_fec_cells/2)] *= np.sqrt(Ps[1])
+        m_tx0[:,int(N_fec_cells/2):] *= np.sqrt(Ps[1])
+        m_tx1[:,int(N_fec_cells/2):] *= np.sqrt(Ps[0])
+        self.tx = m_tx0.flatten() + m_tx1.flatten()*self.phase
