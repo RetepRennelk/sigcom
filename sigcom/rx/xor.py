df6a0ce8b74059b6ad7dce6307c89ff6bfd4cbaa
Peter Klenner <peterklenner@gmx.de>
2018-03-30 10:11:26 +0200

    Simplified binary xor-function.

--- sigcom/rx/xor.py
+++ sigcom/rx/xor.py
@@ -12,14 +12,12 @@ def _clip(inp):
 def XorFwdBwd(Llrs):
     N = len(Llrs)
 
-    bwd = np.zeros(N)
-    bwd[-1] = 1
+    bwd = np.ones(N)
     for k in range(N-1, 0, -1):
         bwd[k-1] = bwd[k] * Llrs[k]
     _clip(bwd)
 
-    fwd = np.zeros(N)
-    fwd[0] = 1
+    fwd = np.ones(N)
     for k in range(N-1):
         fwd[k+1] = fwd[k] * Llrs[k]
     _clip(fwd)
