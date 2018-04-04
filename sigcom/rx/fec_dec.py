a60fea9a8ef8ec5a094fb4f3eea6e77ee22785ed
Peter Klenner <peterklenner@gmx.de>
2018-04-04 16:11:14 +0200

    BI-AWGN SPA fec decoder added.

---
+++ sigcom/rx/fec_dec.py
@@ -0,0 +1,44 @@
+import numpy as np
+from numba import njit
+from sigcom.tx.modcod import LdpcEncAtsc
+from sigcom.rx.softxor import softXorFwdBwd
+
+
+@njit
+def _CN_proc(N, K, indptr, data):
+    # CN Processing
+    for r in range(N - K):
+        fr = indptr[r]
+        to = indptr[r+1]
+        data[fr:to] = softXorFwdBwd(data[fr:to])
+
+
+def fec_dec(Llrs, ldpcEncAtsc, max_iterations):
+    '''
+    FEC decoding with the SPA for LDPC codewords
+    '''
+    K = ldpcEncAtsc.cp.K
+    N_ldpc = ldpcEncAtsc.cp.N
+    N_codewords = len(Llrs) // N_ldpc
+    detected = np.zeros((N_codewords, N_ldpc))
+    m_Llrs = Llrs.reshape(N_codewords, -1)
+
+    for k in range(N_codewords):
+        messages = ldpcEncAtsc.H_dec.copy()
+        messages.data.fill(0)
+
+        for it in range(max_iterations+1):
+            # VN Processing
+            L_sum = m_Llrs[k] + messages.sum(axis=0).A1
+
+            syndrom = ldpcEncAtsc.H_dec.dot(L_sum < 0)
+            N_err = np.sum(np.asarray(syndrom, np.int) % 2)
+            if N_err == 0:
+                break
+                
+            messages.data = L_sum[messages.indices] - messages.data
+
+            _CN_proc(N_ldpc, K, messages.indptr, messages.data)
+
+        detected[k] = L_sum
+    return detected.reshape(-1)
