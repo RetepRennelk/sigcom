5a705f1c70c63b5ec76cf5b3329b878653ce0be2
Peter Klenner <peterklenner@gmx.de>
2018-04-04 13:33:52 +0200

    Added Gpu-class, encapsulated device attributes.

--- sigcom/cuda/util.py
+++ sigcom/cuda/util.py
@@ -1,5 +1,6 @@
 from numba import cuda
 
+
 def print_attr_current_device():
     gpu = cuda.get_current_device()
     print("name = %s" % gpu.name)
@@ -18,3 +19,24 @@ def print_attr_current_device():
     print("unifiedAddressing = %s" % str(gpu.UNIFIED_ADDRESSING))
     print("pciBusID = %s" % str(gpu.PCI_BUS_ID))
     print("pciDeviceID = %s" % str(gpu.PCI_DEVICE_ID))
+
+
+class Gpu():
+    def __init__(self):
+        gpu = cuda.get_current_device()
+        self.name = gpu.name
+        self.MAX_THREADS_PER_BLOCK = gpu.MAX_THREADS_PER_BLOCK
+        self.MAX_BLOCK_DIM_X = gpu.MAX_BLOCK_DIM_X
+        self.MAX_BLOCK_DIM_Y = gpu.MAX_BLOCK_DIM_Y
+        self.MAX_BLOCK_DIM_Z = gpu.MAX_BLOCK_DIM_Z
+        self.MAX_GRID_DIM_X = gpu.MAX_GRID_DIM_X
+        self.MAX_GRID_DIM_Y = gpu.MAX_GRID_DIM_Y
+        self.MAX_GRID_DIM_Z = gpu.MAX_GRID_DIM_Z        
+        self.MAX_SHARED_MEMORY_PER_BLOCK = gpu.MAX_SHARED_MEMORY_PER_BLOCK
+        self.ASYNC_ENGINE_COUNT = gpu.ASYNC_ENGINE_COUNT
+        self.CAN_MAP_HOST_MEMORY = gpu.CAN_MAP_HOST_MEMORY
+        self.MULTIPROCESSOR_COUNT = gpu.MULTIPROCESSOR_COUNT
+        self.WARP_SIZE = gpu.WARP_SIZE
+        self.UNIFIED_ADDRESSING = gpu.UNIFIED_ADDRESSING
+        self.PCI_BUS_ID = gpu.PCI_BUS_ID
+        self.PCI_DEVICE_ID = gpu.PCI_DEVICE_ID
