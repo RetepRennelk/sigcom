88c9f07110831c8eb2027dadc7063fdee41b537b
Peter Klenner <peterklenner@gmx.de>
2018-04-17 17:05:50 +0200

    Put numba's vectorize to good use.

--- sigcom/it/util.py
+++ sigcom/it/util.py
@@ -1,48 +1,42 @@
 import numpy as np
-from numba import njit
+from numba import njit, vectorize
 
 
-@njit
-def getNoisePower(Ias):
+@vectorize
+def getNoisePower(Ia):
     '''
     See bits_to_apriori(bits, Ia) how getNoisePower is put to use
     '''
-    Pas = np.zeros(len(Ias))
-    for k, Ia in enumerate(Ias):
-        if Ia <= .3646:
-            a1 = 1.09542
-            b1 = 0.214217
-            c1 = 2.33727
-            Pa = (a1*Ia**2+b1*Ia+c1*np.sqrt(Ia))**2
-        else:
-            a2 = 0.706692
-            b2 = 0.386013
-            c2 = -1.75017
-            Pa = (-a2*np.log(b2*(1-Ia))-c2*Ia)**2
-        Pas[k] = Pa
-    return Pas
+    if Ia <= .3646:
+        a1 = 1.09542
+        b1 = 0.214217
+        c1 = 2.33727
+        Pa = (a1*Ia**2+b1*Ia+c1*np.sqrt(Ia))**2
+    else:
+        a2 = 0.706692
+        b2 = 0.386013
+        c2 = -1.75017
+        Pa = (-a2*np.log(b2*(1-Ia))-c2*Ia)**2
+    return Pa
 
 
-@njit
-def getMutualInfo(Pas):
-    Ias = np.zeros(len(Pas))
-    for k, Pa in enumerate(Pas):
-        nSigma = np.sqrt(Pa)
-        if nSigma >= 10:
-            Ia = 1
-        elif nSigma <= 1.6363:
-            a1 = -0.0421061
-            b1 = 0.209252
-            c1 = -.00640081
-            Ia = a1*nSigma**3+b1*nSigma**2+c1*nSigma
-        else:
-            a2 = 0.00181491
-            b2 = -0.142675
-            c2 = -0.0822054
-            d2 = 0.0549608
-            Ia = 1-np.exp(a2*nSigma**3+b2*nSigma**2+c2*nSigma+d2)
-        Ias[k] = Ia
-    return Ias
+@vectorize
+def getMutualInfo(Pa):
+    nSigma = np.sqrt(Pa)
+    if nSigma >= 10:
+        Ia = 1
+    elif nSigma <= 1.6363:
+        a1 = -0.0421061
+        b1 = 0.209252
+        c1 = -.00640081
+        Ia = a1*nSigma**3+b1*nSigma**2+c1*nSigma
+    else:
+        a2 = 0.00181491
+        b2 = -0.142675
+        c2 = -0.0822054
+        d2 = 0.0549608
+        Ia = 1-np.exp(a2*nSigma**3+b2*nSigma**2+c2*nSigma+d2)
+    return Ia
 
 
 def mutual_information_magic(Llrs, bits, ldM):
@@ -59,11 +53,7 @@ def mutual_information_magic(Llrs, bits, ldM):
 def bits_to_apriori(bits, Ia):
     '''
     Syntax:
-    bits_to_apriori(bits, [Ia])
-
-    Remark:
-    - Call with [Ia]!
-    - Repeat: Do not forget the [.]!
+    bits_to_apriori(bits, Ia)
     '''
     noise = np.random.randn(len(bits))
     Pa = getNoisePower(Ia)
@@ -78,7 +68,7 @@ def bits_to_multi_apriori(bits, Ias):
     ldM = len(Ias)
     Llrs = np.zeros(N_bits)
     for i, Ia in enumerate(Ias):
-        Pa = getNoisePower([Ia])
+        Pa = getNoisePower(Ia)
         Llrs[i::ldM] = Pa/2*(1-2*bits[i::ldM])+np.sqrt(Pa)*noise[i::ldM]
     return Llrs
 
