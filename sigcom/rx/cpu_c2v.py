eb38f6ad917aeb9381ec09efc6351e8a63141c20
Peter Klenner <peterklenner@gmx.de>
2018-04-18 07:53:53 +0200

    Fixed C2V update on CPU.

--- sigcom/rx/cpu_c2v.py
+++ sigcom/rx/cpu_c2v.py
@@ -17,7 +17,7 @@ def update_c2v(Llrs_ext, C2Vs, code, N_codewords):
                 for q in range(code.nCyclicFactor):
                     a = k*code.N + pcks[i, 0] + (pcks[i, 1]+q) % code.nCyclicFactor
                     fwd[q, i+1] = _partialSoftXor(fwd[q, i],
-                                                  Llrs_ext[a]-C2Vs[k*code.N+N_offset+i, q])
+                                                  Llrs_ext[a]-C2Vs[k*code.N_diags+N_offset+i, q])
 
             bwd = np.zeros((code.nCyclicFactor, len(pcks)))
             bwd[:, -1] = np.inf
@@ -25,11 +25,11 @@ def update_c2v(Llrs_ext, C2Vs, code, N_codewords):
                 for q in range(code.nCyclicFactor):
                     a = k*code.N + pcks[i, 0] + (pcks[i, 1]+q) % code.nCyclicFactor
                     bwd[q, i-1] = _partialSoftXor(bwd[q, i],
-                                                  Llrs_ext[a]-C2Vs[k*code.N+N_offset+i, q])
+                                                  Llrs_ext[a]-C2Vs[k*code.N_diags+N_offset+i, q])
 
             for i in range(N):
                 for q in range(code.nCyclicFactor):
-                    C2Vs[k*code.N+N_offset+i, q] = _partialSoftXor(fwd[q, i], bwd[q, i])
+                    C2Vs[k*code.N_diags+N_offset+i, q] = _partialSoftXor(fwd[q, i], bwd[q, i])
 
             N_offset = (N_offset + len(pcks)) % code.N_diags
 
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     from sigcom.coding.atsc import code_param_long
 
     CR = [8, 15]
-    N_codewords = 1
+    N_codewords = 2
     code = code_param_long.get(CR)
 
     Llrs_ext = np.random.randn(code.N*N_codewords)
